4b9a2473b969221b71a278923ffd98bb17819ff7
samojeyinka <samuelojeyinka@gmail.com>
2023-12-01 09:14:42 -1200

    change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,3 +65,20 @@ class UsersController < ApplicationController
         end
     end
 end
+
+
+git filter-branch --env-filter '
+OLD_EMAIL="cashbankincome@gmail.com"
+CORRECT_NAME="samojeyinka"
+CORRECT_EMAIL="samuelojeyinka@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL" 
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
