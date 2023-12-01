79f6e7b80130722350443c600f0392d8214590ef
Ojeyinka Ifeoluwa Samuel <samuelojeyinka@gmail.com>
2023-12-01 08:06:25 -1200

    Made changes to the before_action in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update, :destroy]
-    before_action :require_user, only: [:edit, :update]
+    before_action :require_user, only: [:show]
     before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
