92867bfab627f1266ef0a86a302f5e004aa7ee7d
samojeyinka <samuelojeyinka@gmail.com>
2023-12-02 23:15:37 -1200

    Added categries index and show, enable category updating for only the admin users

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,19 @@ class CategoriesController < ApplicationController
         end
     end
 
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice] = "Category name updated successfully"
+            redirect_to @category
+        else
+            render 'edit'
+        end
+    end
 
     def index
         @categories = Category.all
@@ -21,6 +34,7 @@ class CategoriesController < ApplicationController
 
     def show
         @category = Category.find(params[:id])
+        @articles = @category.articles.order(created_at: :desc).paginate(page: params[:page], per_page: 6)
     end
 
     private
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-validates :name, presence: true, length:{minimum: 3, maximum: 25}
+validates :name, presence: true, length:{minimum: 3, maximum: 25},uniqueness: { case_sensitive: false }
 validates_uniqueness_of :name
 has_many :article_categories
 has_many :articles, through: :article_categories
