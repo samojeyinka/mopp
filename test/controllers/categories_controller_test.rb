48617ed5dfa4cf8d9632402a09e326a64010b0d0
samojeyinka <samuelojeyinka@gmail.com>
2023-12-04 23:44:25 -1200

    completed the test codes for category

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -39,21 +39,21 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should get edit" do
-  #   get edit_category_url(@category)
-  #   assert_response :success
-  # end
+  test "should get edit" do
+    get edit_category_url(@category)
+    assert_response :success
+  end
 
-  # test "should update category" do
-  #   patch category_url(@category), params: { category: {  } }
-  #   assert_redirected_to category_url(@category)
-  # end
+  test "should update category" do
+    patch category_url(@category), params: { category: {  } }
+    assert_redirected_to category_url(@category)
+  end
 
-  # test "should destroy category" do
-  #   assert_difference("Category.count", -1) do
-  #     delete category_url(@category)
-  #   end
+  test "should destroy category" do
+    assert_difference("Category.count", -1) do
+      delete category_url(@category)
+    end
 
-  #   assert_redirected_to categories_url
-  # end
+    assert_redirected_to categories_url
+  end
 end
